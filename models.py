d72f7f77fb6414590a24fa56e60a11f4e14d532c
Rayan <somaya.rayan2@vodafone.com>
2020-05-13 12:36:18 +0100

    code styling

--- models.py
+++ models.py
@@ -5,15 +5,28 @@ import json
 from datetime import date
 from config import database_setup
 
-#----------------------------------------------------------------------------#
-# Database Setup 
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
+# Database Setup
+# ----------------------------------------------------------------------------#
 
 # Use Production Database.
 # If run locally, key does not exist, so use locally set database instead.
-database_path = os.environ.get('DATABASE_URL', "postgres://{}:{}@{}/{}".format(database_setup["user_name"], database_setup["password"], database_setup["port"], database_setup["database_name"]))
+database_path = os.environ.get(
+                               'DATABASE_URL',
+                               "postgres://{}:{}@{}/{}".format(
+                                                               database_setup
+                                                               ["user_name"],
+                                                               database_setup
+                                                               ["password"],
+                                                               database_setup
+                                                               ["port"],
+                                                               database_setup
+                                                               ["database_name"
+                                                                ]
+                                                               ))
 
 db = SQLAlchemy()
+
 
 def setup_db(app, database_path=database_path):
     '''Binds a flask application and a SQLAlchemy service'''
@@ -23,6 +36,7 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+
 def db_drop_and_create_all():
     '''Drops the database tables to start fresh
        can be used to initialize a clean database
@@ -31,107 +45,115 @@ def db_drop_and_create_all():
     db.create_all()
     db_init_records()
 
+
 def db_init_records():
     '''this is to initialize the database with some test records.'''
     new_actor = (Actor(
-        name = 'Somaya Rayan',
-        gender = 'Female',
-        age = 32
+        name='Somaya Rayan',
+        gender='Female',
+        age=32
         ))
 
     new_movie = (Movie(
-        title = 'Life after COVID19',
-        release_date = date.today()
+        title='Life after COVID19',
+        release_date=date.today()
         ))
 
     new_performance = Performance.insert().values(
-        Movie_id = new_movie.id,
-        Actor_id = new_actor.id,
-        actor_fee = 500.00
+        Movie_id=new_movie.id,
+        Actor_id=new_actor.id,
+        actor_fee=500.00
     )
 
     new_actor.insert()
     new_movie.insert()
-    db.session.execute(new_performance) 
+    db.session.execute(new_performance)
     db.session.commit()
 
-#----------------------------------------------------------------------------#
-# Performance Junction Object N:N 
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
+# Performance Junction Object N:N
+# ----------------------------------------------------------------------------#
+
 
 # Instead of creating a new Table, creating an association table
 Performance = db.Table('Performance', db.Model.metadata,
-    db.Column('Movie_id', db.Integer, db.ForeignKey('movies.id')),
-    db.Column('Actor_id', db.Integer, db.ForeignKey('actors.id')),
-    db.Column('actor_fee', db.Float)
-)
+                       db.Column('Movie_id', db.Integer,
+                                 db.ForeignKey('movies.id')),
+                       db.Column('Actor_id', db.Integer,
+                                 db.ForeignKey('actors.id')),
+                       db.Column('actor_fee', db.Float)
+                       )
 
-#----------------------------------------------------------------------------#
-# Actors Model 
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
+# Actors Model
+# ----------------------------------------------------------------------------#
 
-class Actor(db.Model):  
-  __tablename__ = 'actors'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  gender = Column(String)
-  age = Column(Integer)
+class Actor(db.Model):
+    __tablename__ = 'actors'
 
-  def __init__(self, name, gender, age):
-    self.name = name
-    self.gender = gender
-    self.age = age
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    gender = Column(String)
+    age = Column(Integer)
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name' : self.name,
-      'gender': self.gender,
-      'age': self.age
-    }
+    def update(self):
+        db.session.commit()
 
-#----------------------------------------------------------------------------#
-# Movies Model 
-#----------------------------------------------------------------------------#
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-class Movie(db.Model):  
-  __tablename__ = 'movies'
+    def format(self):
+        return {
+          'id': self.id,
+          'name': self.name,
+          'gender': self.gender,
+          'age': self.age
+        }
 
-  id = Column(Integer, primary_key=True)
-  title = Column(String)
-  release_date = Column(Date)
-  actors = db.relationship('Actor', secondary=Performance, backref=db.backref('performances', lazy='joined'))
+# ----------------------------------------------------------------------------#
+# Movies Model
+# ----------------------------------------------------------------------------#
 
-  def __init__(self, title, release_date) :
-    self.title = title
-    self.release_date = release_date
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+class Movie(db.Model):
+    __tablename__ = 'movies'
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+    actors = db.relationship(
+                             'Actor', secondary=Performance,
+                             backref=db.backref('performances', lazy='joined'))
 
-  def format(self):
-    return {
-      'id': self.id,
-      'title' : self.title,
-      'release_date': self.release_date
-    }
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+          'id': self.id,
+          'title': self.title,
+          'release_date': self.release_date
+        }
